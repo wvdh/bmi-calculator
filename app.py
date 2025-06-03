9fcf219e103740833f102c0ab4e512f972ed10a1
wvdh <wanda@vandenhoogen.eu>
2025-06-03 18:36:49 +0200

    updates api

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template, jsonify
+
+
+from flask import Flask, request, jsonify
+#from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import os
 
